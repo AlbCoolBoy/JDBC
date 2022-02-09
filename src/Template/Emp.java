e9686958100aa55cac6359bda73f1f94160ed9e7
ALB <9752591+shao-mingtao@user.noreply.gitee.com>
2022-02-09 15:05:51 +0800

    convert to JavaBean

---
+++ src/Template/Emp.java
@@ -0,0 +1,100 @@
+package Template;
+
+public class Emp {
+    private Integer customer_id;
+    private String first_name;
+    private String last_name;
+    private String birth_date;
+    private String phone;
+    private String address;
+    private String city;
+    private String state;
+    private Integer points;
+
+    public Integer getCustomer_id() {
+        return customer_id;
+    }
+
+    public void setCustomer_id(Integer customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getBirth_date() {
+        return birth_date;
+    }
+
+    public void setBirth_date(String birth_date) {
+        this.birth_date = birth_date;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStatel() {
+        return state;
+    }
+
+    public void setStatel(String statel) {
+        this.state = statel;
+    }
+
+    public Integer getPoints() {
+        return points;
+    }
+
+    public void setPoints(Integer points) {
+        this.points = points;
+    }
+
+    @Override
+    public String toString() {
+        return "Emp{" +
+                "customer_id=" + customer_id +
+                ", first_name='" + first_name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", birth_date='" + birth_date + '\'' +
+                ", phone='" + phone + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", points=" + points +
+                '}';
+    }
+}
--- src/Template/Template_Demo2.java
+++ src/Template/Template_Demo2.java
@@ -1,6 +1,7 @@
 package Template;
 
 import org.junit.Test;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import 连接池.Util.Druid_Utils;
 
@@ -18,7 +19,6 @@ public class Template_Demo2 {
         String sql = "update t_user set password=? where id=?";
         int count = jdbcTemplate.update(sql,"271824","202012034");
         System.out.println(count);
-
     }
 
     @Test
@@ -42,12 +42,32 @@ public class Template_Demo2 {
         JdbcTemplate jdbcTemplate=new JdbcTemplate(Druid_Utils.getDataSource());
         String sql="select * from customers";
         List<Map<String,Object>> list=jdbcTemplate.queryForList(sql);
-        for(Map<String, Object> stringObjectMap: list){
+        for(Map<String,Object> stringObjectMap: list){
             System.out.println(stringObjectMap);
         }
-
-
     }
+
+    //执行聚合函数
+    @Test
+    public void test05(){
+        JdbcTemplate jdbcTemplate=new JdbcTemplate(Druid_Utils.getDataSource());
+        String sql="select count(last_name) from customers";
+        Long total=jdbcTemplate.queryForObject(sql,Long.class);     //获取聚合函数的返回值
+        System.out.println(total);
+    }
+
+    @Test       //使用template.query将查询结果封装成JavaBean对象
+    public void test06(){
+        JdbcTemplate jdbcTemplate=new JdbcTemplate(Druid_Utils.getDataSource());
+        String sql="select * from customers";
+        List<Emp> list=jdbcTemplate.query(sql,new BeanPropertyRowMapper<Emp>(Emp.class));
+        for (Emp emp : list) {
+            System.out.println(emp);
+        }
+    }
+    //JavaBean对象就是一个具有set和get方法，也就是读写权限的对象
+
+
 }
 
 
